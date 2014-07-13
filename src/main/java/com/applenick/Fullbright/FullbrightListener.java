378f720871227abee4ecf55e8de262e845c2a6b4
applenick <applenick3@me.com>
2014-07-13 09:18:04 -0700

    Fix for respawn, add onConsume method

--- src/main/java/com/applenick/Fullbright/FullbrightListener.java
+++ src/main/java/com/applenick/Fullbright/FullbrightListener.java
@@ -1,10 +1,14 @@
 package com.applenick.Fullbright;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import com.applenick.Fullbright.utils.Alog;
 import com.applenick.Fullbright.utils.PotionUtil;
@@ -20,7 +24,7 @@ public class FullbrightListener implements Listener {
 		if(!(p.hasPlayedBefore())){
 			Fullbright.get().getConfig().addDefault(uuid, false);
 			Fullbright.get().saveConfig();
-			Alog.console(ChatColor.GREEN + p.getName() + "has beedn added to the Fullbright SaveList");
+			Alog.console(ChatColor.GREEN + p.getName() + "has beed added to the Fullbright SaveList");
 		}
 		else if(FullbrightSaveManager.isActivated(p)){
 			PotionUtil.applyNightVison(p);
@@ -32,13 +36,31 @@ public class FullbrightListener implements Listener {
 	}
 	
 	@EventHandler
-	public void onDeath(PlayerRespawnEvent respawnEvent){
-			Player p = respawnEvent.getPlayer();
-			boolean active = FullbrightSaveManager.isActivated(p);
-			
+	public void onConsume(PlayerItemConsumeEvent eat){
+		Player p = eat.getPlayer();
+		boolean active = FullbrightSaveManager.isActivated(p);
+		if(eat.getItem().equals(new ItemStack(Material.MILK_BUCKET))){
 			if(active){
 				PotionUtil.applyNightVison(p);
 			}
+			
+		}
+	}
+	
+	@EventHandler
+	public void onDeath(PlayerRespawnEvent respawnEvent){			
+			final Player p = respawnEvent.getPlayer();
+			boolean active = FullbrightSaveManager.isActivated(p);
+
+			if(active){
+				new BukkitRunnable(){
+					@Override public void run(){
+						PotionUtil.applyNightVison(p);
+					}
+				}.runTaskLater(Fullbright.get(), 4L);
+				
+			}
+			
 		
 	}
 	
