dc4760f40265a5b03bd9f82111176ea1f5d41439
applenick <applenick3@me.com>
2014-09-01 18:01:57 -0700

    Bump to 1.7.10

--- src/main/java/com/applenick/Fullbright/Fullbright.java
+++ src/main/java/com/applenick/Fullbright/Fullbright.java
@@ -18,26 +18,26 @@ import com.sk89q.minecraft.util.commands.MissingNestedCommandException;
 import com.sk89q.minecraft.util.commands.WrappedCommandException;
 
 public class Fullbright extends JavaPlugin {
-	
+
 	public static String prefix = ChatColor.WHITE + "[" + ChatColor.AQUA + "Fullbright" + ChatColor.WHITE + "] ";
-	
-	
+
+
 	private static Fullbright fullbright = null;
 	public static Fullbright get(){
 		return fullbright;
 	}
-	
-	
+
+
 	public void onEnable(){
 		fullbright = this;
-		
+
 		this.getConfig().options().copyDefaults(true);
 		this.saveConfig();
 		this.reloadConfig();
-		
+
 		setup();
 	}
-	
+
 	public void setup(){
 		Alog.console(Alog.dashedChatMessage(ChatColor.AQUA + "FullBright", "#", ChatColor.YELLOW));
 		Alog.console(ChatColor.GOLD +"A Plugin Created by " + ChatColor.DARK_RED +"AppleNick");
@@ -48,43 +48,43 @@ public class Fullbright extends JavaPlugin {
 		Alog.console(ChatColor.GREEN + "Fullbright Listeners have been Registered");
 		Alog.console(ChatColor.GOLD + "Fullbright has been setup.");
 	}
-	
-	 private CommandsManager<CommandSender> commands;
 
-	    private void setupCommands() {
-	        this.commands = new CommandsManager<CommandSender>() {
-	            @Override public boolean hasPermission(CommandSender sender, String perm) {
-	                return sender instanceof ConsoleCommandSender || sender.hasPermission(perm);
-	            }
-	        };
+	private CommandsManager<CommandSender> commands;
 
-	        CommandsManagerRegistration cmdRegister = new CommandsManagerRegistration(this, this.commands);
-	        cmdRegister.register(FullbrightCommand.class);
-	        Alog.console(ChatColor.GREEN + "Fullbright Commands have been setup.");
-	    }
+	private void setupCommands() {
+		this.commands = new CommandsManager<CommandSender>() {
+			@Override public boolean hasPermission(CommandSender sender, String perm) {
+				return sender instanceof ConsoleCommandSender || sender.hasPermission(perm);
+			}
+		};
 
-	    @Override
-	    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-	        try {
-	            this.commands.execute(cmd.getName(), args, sender, sender);
-	        } catch (CommandPermissionsException e) {
-	            sender.sendMessage(ChatColor.RED + "You don't have permission.");
-	        } catch (MissingNestedCommandException e) {
-	            sender.sendMessage(ChatColor.RED + e.getUsage());
-	        } catch (CommandUsageException e) {
-	            sender.sendMessage(ChatColor.RED + e.getMessage());
-	            sender.sendMessage(ChatColor.RED + e.getUsage());
-	        } catch (WrappedCommandException e) {
-	            if (e.getCause() instanceof NumberFormatException) {
-	                sender.sendMessage(ChatColor.RED + "Number expected, string received instead.");
-	            } else {
-	                sender.sendMessage(ChatColor.RED + "An error has occurred. See console.");
-	                e.printStackTrace();
-	            }
-	        } catch (CommandException e) {
-	            sender.sendMessage(ChatColor.RED + e.getMessage());
-	        }
+		CommandsManagerRegistration cmdRegister = new CommandsManagerRegistration(this, this.commands);
+		cmdRegister.register(FullbrightCommand.class);
+		Alog.console(ChatColor.GREEN + "Fullbright Commands have been setup.");
+	}
 
-	        return true;
-	    }
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		try {
+			this.commands.execute(cmd.getName(), args, sender, sender);
+		} catch (CommandPermissionsException e) {
+			sender.sendMessage(ChatColor.RED + "You don't have permission.");
+		} catch (MissingNestedCommandException e) {
+			sender.sendMessage(ChatColor.RED + e.getUsage());
+		} catch (CommandUsageException e) {
+			sender.sendMessage(ChatColor.RED + e.getMessage());
+			sender.sendMessage(ChatColor.RED + e.getUsage());
+		} catch (WrappedCommandException e) {
+			if (e.getCause() instanceof NumberFormatException) {
+				sender.sendMessage(ChatColor.RED + "Number expected, string received instead.");
+			} else {
+				sender.sendMessage(ChatColor.RED + "An error has occurred. See console.");
+				e.printStackTrace();
+			}
+		} catch (CommandException e) {
+			sender.sendMessage(ChatColor.RED + e.getMessage());
+		}
+
+		return true;
+	}
 }
